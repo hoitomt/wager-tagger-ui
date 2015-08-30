c26ebd0795dccee7e707c3182f066f90471e237c
Mike Hoitomt <mike.hoitomt@gmail.com>
2015-08-29 23:52:12 -0500

    Update build artifacts

--- public/javascripts/controllers/home_controllers.js
+++ public/javascripts/controllers/home_controllers.js
@@ -6,8 +6,8 @@ homeControllers.controller("HomeController", [
   '$scope', '$http', '$routeParams', 'Ticket', 'TicketTag', 'Tag', function($scope, $http, $routeParams, Ticket, TicketTag, Tag) {
     var calculateAmountTagged, getTagName, removeTicketTag;
     $scope.tickets = Ticket.range({
-      start_date: '2014-06-01',
-      stop_date: '2015-05-31'
+      start_date: '2015-06-01',
+      stop_date: '2016-05-31'
     });
     $scope.tags = Tag.all();
     $scope.selectedTicketTag = null;
